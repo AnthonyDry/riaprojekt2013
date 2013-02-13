b5332b6dfb47176e029dd5387aac76fcc18752f3
AnthonyDry <anthonydry@live.se>
2013-02-13 01:35:33 +0100

    getting the UI togheter got a bug with not getting changed event from timer.model.

--- demo/js/javascript.js
+++ demo/js/javascript.js
@@ -8,16 +8,49 @@
     return Mustache.compile($('#'+name+'-template').html());
   };
 
- /*
-   FastFingerz.Recipe = Backbone.Model.extend({
-  });
+
   FastFingerz.Storeage = Backbone.Collection.extend({
-    localStorage: new Store("rmystore")
+    localStorage: new Store("mystore")
   });
-*/
+
 	//the worst model in the history of human kind it will change tho! :)
 	FastFingerz.HelloWorld = Backbone.Model.extend({
-		name: function(){ return "helloworld";}
+		name: function(){ return "&nbsp;Fast Fingerz";}
+	});
+	// model for keeping track of the time.
+	FastFingerz.Timer = Backbone.Model.extend({
+		defaults:
+		{
+			"time": "00:00:00",
+			"error":"!ERROR! "
+		},
+		StartTimer:function()
+		{
+			var milliSeconds = 0;
+			var seconds = 0;
+			var minutes = 0;
+			var interval = setInterval(function(){
+			milliSeconds++;
+			if(milliSeconds >= 100)
+			{
+				seconds++;
+				milliSeconds = 0;
+				if(seconds >= 60)
+				{
+					seconds = 0;
+					minutes++;
+					if(minutes >= 100)
+					{
+						//TODO: implement something smart.
+						minutes = 99;
+					}
+				}
+			}
+			this.set('time', minutes+':'+seconds+':'+milliseconds);	
+			},10);
+			
+		},
+		time: ""
 	});
 	
 	//My container view i guess this view will render thruout my application and init other sub views.
@@ -27,12 +60,33 @@
     
     render: function() {
       this.$el.html(this.template(this));
+      var UItimer = new FastFingerz.Index.Timer();
+      this.$('#time_container').append(UItimer.render().el);
       return this;
     },
     name: function() {
       return this.model.name();
     }
   });
+  //View for the timer which instances a new model with keep track of the time. and render it.
+  FastFingerz.Index.Timer = Backbone.View.extend({
+  	template: template("timer"),
+  	model: new FastFingerz.Timer(),
+  	initialize: function(){
+    _.bindAll(this, "render");
+    this.model.bind('all', this.render, this);
+    this.model.StartTimer();
+  },
+  	render:function(){
+  		this.$el.html(this.template(this));
+  		return this;
+  	},
+  	time:function()
+  	{
+  		return this.model.get('time');
+  	}
+  });
+  
   
   FastFingerz.Router = Backbone.Router.extend({
     initialize: function(options) {
